f6730d4cd0b5828bd577b548d362f6f4294607b9
Konrad Niziol <niziol.konrad89@gmail.com>
2020-05-22 21:04:06 +0200

    Add Swagger for document rest api

--- src/main/java/com/kniziol/bank/configuration/SwaggerConfig.java
+++ src/main/java/com/kniziol/bank/configuration/SwaggerConfig.java
@@ -1,4 +1,9 @@
 package com.kniziol.bank.configuration;
 
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
 public class SwaggerConfig {
 }
